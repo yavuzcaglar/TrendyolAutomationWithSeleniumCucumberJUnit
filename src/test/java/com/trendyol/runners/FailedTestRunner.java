beb5ec865daf1e866fd05fd9a5a14725787ed4f0
Yavuz <yavuzcglrr@gmail.com>
2023-07-11 14:29:12 +0300

    Page Object Model created.

---
+++ src/test/java/com/trendyol/runners/FailedTestRunner.java
@@ -0,0 +1,16 @@
+package com.trendyol.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/failed-html-reports"
+        },
+        features = "@target/rerun.txt",
+        glue ="com/trendyol/step_definitions"
+)
+public class FailedTestRunner {
+}
